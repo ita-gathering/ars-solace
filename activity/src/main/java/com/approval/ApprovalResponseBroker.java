04ea1fb6c078742ce6b00142d4b51f4c07c56cec
Ocean Liang <ocean.liang@cargosmart.com>
2019-04-25 15:25:06 +0800

    approval  service can send message to activity service

--- activity/src/main/java/com/approval/ApprovalResponseBroker.java
+++ activity/src/main/java/com/approval/ApprovalResponseBroker.java
@@ -1,11 +1,7 @@
 package com.approval;
 
 import com.approval.solace.MessageSender;
-import com.approval.solace.SolaceBrokerException;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.annotation.JmsListener;
-import org.springframework.retry.annotation.Backoff;
-import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
@@ -15,23 +11,18 @@ import javax.jms.TextMessage;
  * @author Ocean Liang
  * @date 4/25/2019
  */
-
 @Component
-@Slf4j
-public class ParticipateApprovalBroker {
-    private final String LISTENED_QUEUE = "ARS/ACTIVITY/EVT";
+public class ApprovalResponseBroker {
+    private final String LISTENED_QUEUE = "ARS/APPROVAL/EVT";
     private static final String DEFAULT_CONCURRENCY = "10";
     private static final String QUEUE_LISTENER_FACTORY = "queueListenerFactory";
-    private static final String OUTPUT_TOPIC = "ARS/APPROVAL";
 
     @Resource
     private MessageSender messageSender;
 
-//    @Retryable(value = {SolaceBrokerException.class}, backoff = @Backoff(random = true, multiplier = 0))
+    //    @Retryable(value = {SolaceBrokerException.class}, backoff = @Backoff(random = true, multiplier = 0))
     @JmsListener(destination = LISTENED_QUEUE, containerFactory = QUEUE_LISTENER_FACTORY, concurrency = DEFAULT_CONCURRENCY)
     public void processMessage(TextMessage originalMessage) throws Exception {
-        System.out.println("receive from queue");
         System.out.println(originalMessage.getText());
-        messageSender.sendMessageToTopic(OUTPUT_TOPIC,"send to activity");
     }
 }
